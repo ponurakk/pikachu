8c568967479152b4a710f810f321f251df5db9d1
ponurakk <91278893+ponurakk@users.noreply.github.com>
2021-10-05 20:40:26 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -64,10 +64,10 @@ function updateGameArea() {
     myGameArea.clear();
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0; 
-    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -1; }
-    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 1; }
-    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -1; }
-    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 1; }
+    if (myGameArea.key && myGameArea.key == 37) {myGamePiece.speedX = -5; }
+    if (myGameArea.key && myGameArea.key == 39) {myGamePiece.speedX = 5; }
+    if (myGameArea.key && myGameArea.key == 38) {myGamePiece.speedY = -5; }
+    if (myGameArea.key && myGameArea.key == 40) {myGamePiece.speedY = 5; }
     myGamePiece.newPos();
     myGamePiece.update();
 }
